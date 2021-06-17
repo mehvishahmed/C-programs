c99c0300930a35a0b9e9ba882a8adaee09f3a9fd
Mehvish Ahmed <84987764+mehvishahmed@users.noreply.github.com>
2021-06-17 19:26:36 +0530

    Drawing a shape

---
+++ Practice Programs/Drawing a shape.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    printf("Drawing a shape using printf statement\n\n");
+    printf("   /|\n");
+    printf("  / |\n");
+    printf(" /  |\n");
+    printf("/___|\n");
+    return 0;
+}
