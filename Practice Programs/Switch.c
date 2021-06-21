5b69621bd71746f4fb6263db4fb27a2cafe415c4
Mehvish Ahmed <84987764+mehvishahmed@users.noreply.github.com>
2021-06-21 19:32:42 +0530

    Switch Statement
    Print Remarks based on a student's grade

---
+++ Practice Programs/Switch.c
@@ -0,0 +1,29 @@
+//SWITCH STATEMENT
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char grade = 'B';   //Grade of the student
+    switch(grade)
+    {
+    case 'A':
+        printf("You did great!");
+        break;
+    case 'B':
+        printf("You did alright.");
+        break;
+    case 'C':
+        printf("You did poorly.");
+        break;
+    case 'D':
+        printf("You did very bad.");
+        break;
+    case 'F':
+        printf("You failed!");
+        break;
+    default:
+        printf("INVALID GRADE.");
+        break;
+    }
+    return 0;
+}
