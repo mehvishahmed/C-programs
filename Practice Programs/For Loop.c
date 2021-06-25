b89fb061a488d0b79dd07a4ff23d557f97bd414c
Mehvish Ahmed <84987764+mehvishahmed@users.noreply.github.com>
2021-06-25 23:09:35 +0530

    For Statement
    Printing all elements in an array using for statement

---
+++ Practice Programs/For Loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int luckyNum[] = {1, 4, 15, 16, 23, 42};
+    int i;
+    for(i=0; i<6;i++)
+    {
+        printf("%d\n", luckyNum[i]);
+    }
+    return 0;
+}
