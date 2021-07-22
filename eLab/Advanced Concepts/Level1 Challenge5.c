bc5c1cf5274efdaaca425ffb3086857fd1162268
Mehvish Ahmed <84987764+mehvishahmed@users.noreply.github.com>
2021-07-22 22:49:57 +0530

    Level1 Challenge5

---
+++ eLab/Advanced Concepts/Level1 Challenge5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define mod 1000000007
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--){
+    long long unsigned int x,y;
+    scanf("%llu %llu",&x,&y);
+    int a=x;
+  //  int sum=x;
+    int i;
+    for(i=0;i<y-1;i++){
+        x=(a*x)%mod;
+       // sum=(sum+x)%mod;
+    }
+    printf("%llu\n",x);}
+    
+    
+
+	return 0;
+}
