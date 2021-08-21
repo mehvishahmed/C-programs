12e87d082d728a6ee7d9f05a7a0b41c76e8a8303
Mehvish Ahmed <84987764+mehvishahmed@users.noreply.github.com>
2021-08-21 23:53:17 +0530

    Using gets() and puts() to read and print strings

---
+++ Practice Programs/gets() and puts().c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char line[30];
+    puts("Enter a line: ");
+    gets(line);
+    puts("\nThe line you have entered is: \n");
+    puts(line);
+    return 0;
+}
