6294470513a92b7b3a3a6a5a58c85cadfbe4b491
Mehvish Ahmed <84987764+mehvishahmed@users.noreply.github.com>
2021-09-30 19:42:48 +0530

    Level2 Challenge9

---
+++ eLab/Advanced Concepts/Level2 Challenge9.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    long long int a[10000],temp;
+    long long int sum1=0,med,median,ans=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%lld",&a[i]);
+        sum1+=a[i];
+    }
+    med=sum1/n;
+    for(i=0;i<n;i++)
+        a[i]-=med;
+    for(i=0;i<n;i++)
+        a[i]+=a[i-1];
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[j]<a[i])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    if(n%2!=0)
+        median=a[n/2];
+    else
+        median=(a[n/2]+a[n/2-1])/2;
+    for(i=0;i<n;i++)
+    {
+        a[i]-=median;
+        if(a[i]<0)
+            a[i]*=-1;
+        ans+=a[i];
+    }
+    printf("%lld",ans);
+	return 0;
+}
